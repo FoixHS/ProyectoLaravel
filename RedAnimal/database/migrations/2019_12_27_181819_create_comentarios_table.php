dfb6d876cfd6c588f154b7e20db164e086945559
unknown <ignaciocivetta@gmail.com>
2019-12-27 15:24:54 -0300

    Migraciones

---
+++ RedAnimal/database/migrations/2019_12_27_181819_create_comentarios_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComentariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comentarios', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('posteo_id');
+            $table->unsignedBigInteger('usuario_id');
+            $table->longText('texto');
+            $table->timestamp('date')->useCurrent();
+            $table->foreign('posteo_id')->references('id')->on('posteos');
+            $table->foreign('usuario_id')->references('id')->on('usuarios');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comentarios');
+    }
+}
